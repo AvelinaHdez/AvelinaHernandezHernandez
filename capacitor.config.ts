09cac41945945e70305612f5d259fe798eb6c500
Mac <mac@Macs-MacBook-Pro.local>
2024-10-15 10:07:54 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'avelina-hernandez-hernandez-10a',
+  webDir: 'dist'
+};
+
+export default config;
